52bd1936b9f35f90bc68bf5fd5984604007ec4a8
hahahihiii <111178435+hahahihiii@users.noreply.github.com>
2022-10-22 16:36:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const nameGirl = 'NHAT';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'happybirthdaytoyou.ga';
 const eventName = 'Happy birthday';
 const titleCard = 'Happy birthday to you';
 const contentCard = 'Happy birthday to you and send me your youtube channel XD';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.6435-9/97966058_113737757015523_3159444046779252736_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=wcaFO6oDljwAX_Mw8n0&tn=Jrc1SGXiRsPLEFvt&_nc_ht=scontent.fsgn5-2.fna&oh=00_AT_X1sua-kLlLKqAFXJPAvQ3-D3RCGgCouvitNNPyW3YsA&oe=637A056F';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
